f87559a5f214f7f5cbe97275376db85c775e1409
Triana Cerda <trianaRcerda@gmail.com>
2021-08-11 17:50:48 -0700

    passing and working evenOnly func, fiveCharOrFewerOnly, pplInIll

--- src/challenge02/codeChallenge02.js
+++ src/challenge02/codeChallenge02.js
@@ -1,8 +1,24 @@
 export function fiveAndGreaterOnly(arr) {
     return arr.filter((num) => {
-       if (num > 5) {
+       if (num >= 5) {
            return num;
        }
     })
 }
 
+export function evenOnly(arr) {
+    return arr.filter((num) => num % 2 === 0);
+}
+
+export function fiveCharactersOrFewerOnly(arr) {
+  return arr.filter((item) => {
+    return item.length <= 5;
+    });
+  }
+
+  export function peopleWhoBelongToTheIlluminati(arr){
+   return arr.filter((ppl) => {
+       if (ppl.member === true)
+       return ppl;
+   });
+}
--- src/challenge02/codeChallenge2.test.js
+++ src/challenge02/codeChallenge2.test.js
@@ -1,4 +1,4 @@
-import { fiveAndGreaterOnly } from './codeChallenge02';
+import { fiveAndGreaterOnly, evenOnly, fiveCharactersOrFewerOnly, peopleWhoBelongToTheIlluminati } from './codeChallenge02';
 
 test('Five and greater only', () => {
   const input = [3, 6, 8, 2]; // arrange
@@ -6,8 +6,30 @@ test('Five and greater only', () => {
   expect(output).toEqual([6, 8]); // assert
 });
 
-// test('even only', () => {
-//     const input = [3, 6, 8, 2]; // arrange
-//     const output = fiveAndGreaterOnly(input); // act
-//     expect(output).toEqual([6, 8]); // assert
-//   });
+test('even only', () => {
+    const input = [3, 6, 8, 2]; // arrange
+    const output = evenOnly(input); // act
+    expect(output).toEqual([6, 8, 2]); // assert
+  });
+
+  test('five chars or fewer only', () => {
+    const input = ['dog', 'wolf', 'by', 'family', 'eaten', 'camping']; // arrange
+    const output = fiveCharactersOrFewerOnly(input); // act
+    expect(output).toEqual(['dog', 'wolf', 'by', 'eaten']); // assert
+  });
+
+  test('ppl belong in illuminati', () => {
+    const input = [
+      { name: 'Angelina Jolie', member: true },
+      { name: 'Eric Jones', member: false },
+      { name: 'Paris Hilton', member: true },
+      { name: 'Kayne West', member: false },
+      { name: 'Bob Ziroll', member: true }
+  ]; // arrange
+    const output = peopleWhoBelongToTheIlluminati(input); // act
+    expect(output).toEqual([ 
+      { name: 'Angelina Jolie', member: true }, 
+      { name: 'Paris Hilton', member: true }, 
+      { name: 'Bob Ziroll', member: true } 
+  ]); // assert
+  });
