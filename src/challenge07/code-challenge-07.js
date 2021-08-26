ec819e81a988be33edaba62f5f94e0965fb0e5e2
Triana Cerda <trianaRcerda@gmail.com>
2021-08-26 16:08:11 -0700

    attempted code challenge 7

---
+++ src/challenge07/code-challenge-07.js
@@ -0,0 +1,52 @@
+// const sortByChildren = (charArray) => {
+//     let characters = [
+//         {
+//           name: 'Eddard',
+//           spouse: 'Catelyn',
+//           children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+//           house: 'Stark'
+//         },
+//         {
+//           name: 'Jon A.',
+//           spouse: 'Lysa',
+//           children: ['Robin'],
+//           house: 'Arryn'
+//         },
+//         {
+//           name: 'Cersei',
+//           spouse: 'Robert',
+//           children: ['Joffrey', 'Myrcella', 'Tommen'],
+//           house: 'Lannister'
+//         },
+//         {
+//           name: 'Daenarys',
+//           spouse: 'Khal Drogo',
+//           children: ['Drogon', 'Rhaegal', 'Viserion'],
+//           house: 'Targaryen'
+//         },
+//         {
+//           name: 'Mace',
+//           spouse: 'Alerie',
+//           children: ['Margaery', 'Loras'],
+//           house: 'Tyrell'
+//         },
+//         {
+//           name: 'Euron',
+//           spouse: null,
+//           children: [],
+//           house: 'Greyjoy'
+//         },
+//         {
+//           name: 'Jon S.',
+//           spouse: null,
+//           children: [],
+//           house: 'Snow'
+//         }
+//       ];
+// };
+
+// const containsW = (str) => {
+//     return str.match(containsW)
+// };
+
+//I really tried to tackle this but whoa-- the first challenge was I couldn't even get the test working and wasn't entirely sure where to put characters. I attempted the second one and feel confident it needs to be a .match but the beggining const doesn't make sense to me. When I was coding it was register on/off with eslint and wasn't sure how to return it. 
